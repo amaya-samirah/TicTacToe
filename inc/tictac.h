ab3759b2681725d305004c5918621cd9b7af62d8
Amaya Shabazz <amaya.s.26292@gmail.com>
2025-11-08 01:08:41 -0500

    Added to header file

--- inc/tictac.h
+++ inc/tictac.h
@@ -10,6 +10,25 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+struct Board {
+    int board[3][3];
+};
 
+// Draws the board
+void Draw();
+
+// Resets the game and board
+void Reset(Board &board);
+
+// Start the game loop
+void StartGame();
+
+// Updates the board after player turns
+// Returns a bool if the game is over
+bool UpdateBoard(Board &currBoard);
+
+// Checks if a player has won
+// Returns the player's number if they have won, else returns a 0
+int CheckWin(Board &currBoard);
 
 #endif  // TICTACTOE_INC_TICTAC_H_
